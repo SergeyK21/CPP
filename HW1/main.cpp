38110b4a7959eb50245c019b295afe49177d8d1d
Sergey <isip88g@gmail.com>
2023-11-23 22:07:25 +0300

    HW1

---
+++ HW1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // здесь вычисляем числа фибоначи 
+    double f1 = 1.0;
+    double f2 = 1.0;
+    double f3 = 2.0;
+    double f4 = 3.0;
+    double f5 = 5.0;
+
+    // здесь осуществляется вывод найденных чисел в консоль
+    std::cout << "Fibonacchi number 1 : " << f1 << std::endl;
+    std::cout << "Fibonacchi number 2 : " << f2 << std::endl;
+    std::cout << "Fibonacchi number 3 : " << f3 << std::endl;
+    std::cout << "Fibonacchi number 4 : " << f4 << std::endl;
+    std::cout << "Fibonacchi number 5 : " << f5 << std::endl;
+}
